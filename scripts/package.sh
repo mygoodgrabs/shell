6612bbcc8631385ebe0906303fd5ebe958ae3e7a
Amaury Martiny <amaury.martiny@gmail.com>
2018-03-07 14:52:16 +0100

    Remove useless stuff

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,7 +3,7 @@
 BRANCH=$CI_BUILD_REF_NAME
 
 # Build binaries with electron
-if [ "$BRANCH" == "next" ]; then
+if [ "$BRANCH" == "am-testing" ]; then
   if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then npm run package; fi
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then npm run package -- --mac --win; fi
 fi
